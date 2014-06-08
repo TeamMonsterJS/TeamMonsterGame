6c3419e1576564605fd836e0521faffb474c09d2
Yasen <ias_sv_bg@yahoo.co.uk>
2014-06-08 10:48:49 +0300

    changes and more tests on drawing + levels to be hardcoded

--- TeamMonsterPacMan/PacMan/js/engine.js
+++ TeamMonsterPacMan/PacMan/js/engine.js
@@ -2,7 +2,8 @@
     // here we will have the pressed direction eventListener
     // Sets event listener for the pressed keyes
     window.addEventListener("keydown", keyPressed, true);
-    var i,
+    var dots = [],
+        i,
         j;
 
     function keyPressed(e) {
@@ -32,20 +33,29 @@
             paper.image(ghost.appearance, ghost.position.x, ghost.position.y, 50, 50);
         }
 
-        this.renderPacMan = function (pacMan) {
+        // TODO: Needs to be made better is now just for rendering a sample player
+        this.renderPacMan = function (pacMan, direction) {
+            var playerRadius = 8,
+                drawingShiftFromPositionTopLeft = 10;
 
+            paper.circle(pacMan.position.x + drawingShiftFromPositionTopLeft, pacMan.position.y + drawingShiftFromPositionTopLeft, playerRadius)
+                .attr({
+                    fill: 'yellow',
+                    stroke: 'none'
+                })
         }
 
         this.renderPacDots = function (dots) {
             for (var dot in dots) {
-                var dotRadius = 5,
-                    color = 'yellow';
+                var dotRadius = 3,
+                    color = 'yellow',
+                    drawingShiftFromPositionTopLeft = 10;
 
                 if (dots[dot].isPowerDot) {
                     dotRadius = 10;
                     color = 'white';
                 }
-                paper.circle(dots[dot].position.x, dots[dot].position.y, dotRadius)
+                paper.circle(dots[dot].position.x + drawingShiftFromPositionTopLeft, dots[dot].position.y + drawingShiftFromPositionTopLeft, dotRadius)
                     .attr({
                         fill: color,
                         stroke: 'orange'
@@ -54,13 +64,14 @@
         }
 
         this.erasePacDot = function (dot) {
-            var dotRadius = 5;
+            var dotRadius = 3,
+                drawingShiftFromPositionTopLeft = 10;
 
             if (dot.isPowerDot) {
                 dotRadius = 10;
             }
 
-            paper.circle(dot.position.x, dot.position.y, dotRadius)
+            paper.circle(dot.position.x + drawingShiftFromPositionTopLeft, dot.position.y + drawingShiftFromPositionTopLeft, dotRadius)
                 .attr({
                     fill: 'black',
                     stroke: 'black'
@@ -69,7 +80,7 @@
 
         // Needs to be reformed or totali changed depending on how we will input the level
         // For now works with hardcoded levels, and maybe some drawing changes are much needed
-        this.renderLevel = function (level) {
+        this.renderLevel = function (level, dots) {
             var step = 20;
             for (i = 0; i < 420; i += step) {
                 for (j = 0; j < 420; j += step) {
@@ -78,11 +89,15 @@
                             .attr({
                                 fill: 'purple'
                             });
+                    } else {
+                        dots.push(new PacDot(new Position(j, i)));
                     }
+
                 }
             }
         }
     }
 
     this.renderer = new Renderer();
+
 }
---
+++ TeamMonsterPacMan/PacMan/js/levels.js
@@ -0,0 +1,20 @@
+﻿// HARD CODING levels options
+function level1(matrix) {
+    var level = matrix,
+        i,
+        j,
+        step = 20;
+
+    // level borders
+    for (i = 0; i < matrix.length; i += step) {
+        
+        for (j = 0; j < matrix.length; j+=step) {
+            if (i > 20 && i < matrix.length - step) {
+                continue;
+            }
+            level[i][j] = true;
+        }
+    }
+    console.log(level);
+    return level;
+}
