bb7adce412f5831c19d45df7dbf46e245d7bb6cf
yordan-sp <yordan_sp@hotmail.com>
2014-06-10 20:10:34 +0300

    Changed size of the status block.

--- TeamMonsterPacMan/PacMan/js/menu.js
+++ TeamMonsterPacMan/PacMan/js/menu.js
@@ -1,8 +1,8 @@
 ﻿//test comment
 var menu = new Kinetic.Stage({
     container: 'canvas-container',
-    width: 450,
-    height: 350
+    width: 350,
+    height: 250
 });
 
 var layer = new Kinetic.Layer();
@@ -18,32 +18,32 @@ var base = new Kinetic.Rect({
     y: 0,
     width: menu.width(),
     height: menu.height(),
-    strokeWidth:15
+    strokeWidth: 15
 });
 
 var scoreBoard = new Kinetic.Text({
     x: base.x() + 15,
     y: base.y() + 20,
     text: 'Current score',
-    fontSize: 50,
+    fontSize: 30,
     fill: '#A3A000',
     fontFamily: 'Fixedsys'
 });
 
 var scoreCount = new Kinetic.Text({
     x: scoreBoard.x(),
-    y: scoreBoard.y() + 60,
+    y: scoreBoard.y() + 40,
     text: '0',
-    fontSize: 50,
+    fontSize: 30,
     fill: '#A3A000',
     fontFamily: 'Fixedsys'
 });
 
 var livesText = new Kinetic.Text({
     x: scoreBoard.x(),
-    y: scoreCount.y() +80,
+    y: scoreCount.y() + 60,
     text: 'Lives',
-    fontSize: 50,
+    fontSize: 30,
     fill: '#A3A000',
     fontFamily: 'Fixedsys'
 });
@@ -51,28 +51,28 @@ var livesText = new Kinetic.Text({
 //TODO
 //Find optimized way to draw 3 hearts
 var heart = new Kinetic.Text({
-    x: livesText.x()+200,
-    y: scoreCount.y() +80,
+    x: livesText.x() + 200,
+    y: scoreCount.y() + 60,
     text: '\u2665',
-    fontSize: 50,
+    fontSize: 30,
     fill: '#B52D1B',
     fontFamily: 'Fixedsys'
 });
 
 var startButton = new Kinetic.Text({
-    x: livesText.x()+20,
-    y: livesText.y() + 100,
+    x: livesText.x() + 20,
+    y: livesText.y() + 80,
     text: 'START',
-    fontSize: 50,
+    fontSize: 30,
     fill: '#A3A000',
     fontFamily: 'Fixedsys'
 });
 
 var pauseButton = new Kinetic.Text({
     x: startButton.x() + 200,
-    y: livesText.y() + 100,
+    y: livesText.y() + 80,
     text: 'PAUSE',
-    fontSize: 50,
+    fontSize: 30,
     fill: '#A3A000',
     fontFamily: 'Fixedsys'
 });
